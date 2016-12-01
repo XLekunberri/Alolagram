caf1c63d5155fa0909c8890ab27a16d398b8f233
XLekunberri <xlekunberri@gmail.com>
2016-12-01 16:55:57 +0100

    Aldaketa batzuk

--- erregistratu.php
+++ erregistratu.php
@@ -2,11 +2,11 @@
 
 $DATU_BASEA='resources/erabiltzaileak.xml';
 
-if(libre_dago($_POST['erabiltzailea'])) {
+if(libre_dago($_POST['izena'])) {
     $erabiltzaile_guztiak = simplexml_load_file($DATU_BASEA);
 
-    $erabiltzaile_hau = $erabiltzaile_guztiak->addChild('erabiltzailea');
-    $erabiltzaile_hau->addChild(izena, $_POST['erabiltzailea']);
+    $erabiltzaile_hau = $erabiltzaile_guztiak->addChild('izena');
+    $erabiltzaile_hau->addChild(izena, $_POST['izena']);
     $erabiltzaile_hau->addChild(pasahitza, $_POST['pasahitza']);
 
     $erabiltzaile_guztiak->asXML($DATU_BASEA);
@@ -15,13 +15,14 @@ else{
     alert("Erabiltzaile hori existitzen da.");
 }
 
-function libre_dago($_POST['erabiltzailea']){
+function libre_dago($erabiltzailea){
     global $DATU_BASEA;
+
     $erabiltzaile_guztiak = simplexml_load_file($DATU_BASEA);
     $libre = true;
 
-    foreach ($erabiltzaile_guztiak->erabiltzailea['izena'] as $egungoa){
-        if($egungoa == $_POST['erabiltzailea']){
+    foreach ($erabiltzaile_guztiak->erabiltzaileak->erabiltzailea['izena'] as $egungoa){
+        if($egungoa == $erabiltzailea){
             $libre = false;
         }
     }
