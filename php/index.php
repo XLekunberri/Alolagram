2e0dfbef7ab4d33a6739e7302c3e5caca1c4600a
Tiza <ikerzalla@gmail.com>
2016-12-03 22:16:45 +0100

    Testu-kutxetan transparentzia

--- php/index.php
+++ php/index.php
@@ -4,9 +4,28 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <title>Alolagram</title>
     <link rel="shortcut icon" type="image/x-icon" href="../resources/img/favicon.ico"/>
-    <link rel="stylesheet" href="../css/erregistratu.css" type="text/css"/>
-    <script type="text/javascript" src="../js/erregistratu.js"></script>
+    <link rel="stylesheet" href="../css/index.css" type="text/css"/>
 </head>
 <body>
-<a href="login.php">Login</a>
-<a href="erregistratu.php">Eregistroa</a>
+<img src="../resources/img/pokemon.png" id="titulua" height="200px">
+<div id="kontua">
+    <a href="login.php" class="testua">Login</a></br></br>
+    <a href="erregistratu.php" class="testua">Erregistroa</a>
+</div>
+<div id="argazkiak">
+    <img src="../resources/img/bellosom.gif" id="bellosom" class="argazkia">
+    <img src="../resources/img/ocean.gif" id="ocean" class="argazkia">
+</div>
+<div id="azalpena">
+    <h1>
+        Kaixo, ongi etorri Alolagram-era!
+    </h1>
+    <p>
+        Hemen <i>Alola</i> lurraldean zehar ateratako hainbat argazki ikusi ahalko dituzu, non <i>Pokemon</i> asko haien ingurean ikusi ahalko dituzun.
+        Gainera, argazkietan zure iruzkina utz dezakezu mundu guztiak jakin dezan argazki bat gustatu zaizun!
+    </p>
+    <p>
+        Orrialde hau erabiltzeko logeatu behar zara zure kontuarekin; horretarako hemen goian dagoen "Login" botoian sakatu.
+        Kontu bat ez baduzu, "Erregistratu" botoian sakatu pnatailaren goikaldean.
+    </p>
+</div>
