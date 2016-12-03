10de237e19808322540392c80a9faf20547703f4
XLekunberri <xlekunberri@gmail.com>
2016-12-03 12:41:00 +0100

    gallery.php ia bukatuta

--- php/gallery.php
+++ php/gallery.php
@@ -4,23 +4,31 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <title>Alolagram</title>
     <link rel="shortcut icon" type="image/x-icon" href="../resources/img/favicon.ico"/>
-    <link rel="stylesheet" href="erregistratu.css" type="text/css"/>
-    <script type="text/javascript" src="erregistratu.js"></script>
+    <link rel="stylesheet" href="../css/gallery.css" type="text/css"/>
+    <script type="text/javascript" src="../css/gallery.js"></script>
 </head>
 <body>
-    <div class="nagusi">
+<div class="nagusi">
     <?php
     session_start();
-    $id = $_GET['id'];
-    $erabiltzailea = $_SESSION['erabiltzailea'];
+    if(!isset($_SESSION['erabiltzailea'])){
+        echo "<script type='text/javascript'>window.location = \"index.php\";</script>";
+    }
+    elseif(!isset($_GET['id'])){
+        echo "<script type='text/javascript'>window.location = \"alola.php\";</script>";
+    }
+    else {
+        $id = $_GET['id'];
+        $erabiltzailea = $_SESSION['erabiltzailea'];
 
-    //Argazkia gehitu
-    echo "<img src='../resources/img/gallery/$helbidea$id.png' id=argazkia'>";
+        //Argazkia gehitu
+        echo "<img src='../resources/img/gallery/$helbidea$id.png' id=\"argazkia\">";
 
 
-    testuaGehitu($id);
-
-    function testuaGehitu($id){
+        testuaGehitu($id);
+    }
+    function testuaGehitu($id)
+    {
 
 
         echo "<div class='testua'>";
@@ -30,13 +38,19 @@
         //Idazlea gehitu
         idazleaGehitu($iruzkinak);
 
+        echo "<hr/>";
+
         //Pertsonen iruzkinak gehitu
         iruzkinakGehitu($iruzkinak);
+
+        //Iruzzkinak idazteko tokia gehitu
+        textfieldGehitu();
 
         echo "</div>";
     }
 
-    function idazleaGehitu($db){
+    function idazleaGehitu($db)
+    {
         echo "<div class='egilea'>";
 
         $izena = $db['izena'];
@@ -44,18 +58,19 @@
         $data = $db['data'];
         $iruzkina = $db['iruzkina'];
 
-        echo "$izena</br>";
-        echo "$data</br>";
+        echo "$izena";
+        echo " - $data</br>";
         echo "$iruzkina";
 
         echo "</div>";
     }
 
-    function iruzkinakGehitu($db){
+    function iruzkinakGehitu($db)
+    {
 
         echo "<div class='iruzkinak'>";
 
-        foreach($db->children() as $elem){
+        foreach ($db->children() as $elem) {
             echo "<div class='iruzkina'>";
 
             $izena = $elem->izena;
@@ -63,17 +78,26 @@
             $data = $elem->data;
             $iruzkina = $elem->iruzkina;
 
-            echo "$izena</br>";
-            echo "$data</br>";
+            echo "$izena";
+            echo " - $data</br>";
             echo "$iruzkina";
 
             echo "</div>";
+            echo "<hr/>";
         }
 
         echo "</div>";
     }
 
+    function textfieldGehitu(){
+        echo "<div class='idazteko'>";
+        echo "<form action=\"iruzkinaGorde.php\" method=\"post\">";
+        echo "<textarea name=\"iruzkina\" rows=\"3\" cols=\"71\" placeholder=\"Idatz ezazu zure iruzkina hemen\"></textarea><br>";
+        echo "<button type=\"submit\"  onclick=\"return iruzkinaGorde(this.form);\">Bidali</button>";
+        echo "</form>";
+        echo "</div>";
+    }
     ?>
-    </div>
+</div>
 </body>
 </html>
