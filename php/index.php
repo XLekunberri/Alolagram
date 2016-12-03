6287617ce1920299400947d8617ab53678b37bec
Tiza <ikerzalla@gmail.com>
2016-12-03 20:23:25 +0100

    HTML karaktere bereziak ez dira erabiltzen iruzkina gordetzean 2.0

--- php/index.php
+++ php/index.php
@@ -1,1 +1,12 @@
-<a href="login.php">login</a>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <title>Alolagram</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../resources/img/favicon.ico"/>
+    <link rel="stylesheet" href="../css/erregistratu.css" type="text/css"/>
+    <script type="text/javascript" src="../js/erregistratu.js"></script>
+</head>
+<body>
+<a href="login.php">Login</a>
+<a href="erregistratu.php">Eregistroa</a>
