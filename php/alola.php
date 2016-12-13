b5cff476d7d16999076db9135694e585b976f07f
XLekunberri <xlekunberri@gmail.com>
2016-12-13 18:51:38 +0100

    Loginetik bug bat konpondua

--- php/alola.php
+++ php/alola.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html" charset=UTF-8"/>
     <title>Alolagram</title>
     <link rel="shortcut icon" type="image/x-icon" href="../resources/img/favicon.ico"/>
     <link rel="stylesheet" href="../css/alola.css" type="text/css"/>
@@ -15,16 +15,16 @@
     ?>
     <img src="../resources/img/alola_map.jpg" width="860" height="480" id="mapa">
     <div class="poke">
-        <a href="gallery.php?id=001"><img src="../resources/img/pokeball.png" width="16" height="16" id="poke1"></a>
+        <a href="gallery.php?id=001"><img src="../resources/img/pokeball.png" alt="poke1" width="16" height="16" id="poke1"></a>
     </div>
     <div class="poke">
-        <a href="gallery.php?id=002"><img src="../resources/img/pokeball.png" width="16" height="16" id="poke2"></a>
+        <a href="gallery.php?id=002"><img src="../resources/img/pokeball.png" alt="poke2" width="16" height="16" id="poke2"></a>
     </div>
     <div class="poke">
-        <a href="gallery.php?id=003"><img src="../resources/img/pokeball.png" width="16" height="16" id="poke3"></a>
+        <a href="gallery.php?id=003"><img src="../resources/img/pokeball.png" alt="poke3" width="16" height="16" id="poke3"></a>
     </div>
     <div class="poke">
-        <a href="gallery.php?id=004"><img src="../resources/img/pokeball.png" width="16" height="16" id="poke4"></a>
+        <a href="gallery.php?id=004"><img src="../resources/img/pokeball.png" alt="poke4" width="16" height="16" id="poke4"></a>
     </div>
 
     <div id="validator">
